8841ac48b99fcb371f2c14bf11393607e04a4bfc
krapaomukrob <Pannathorn3116@gmail.com>
2022-06-24 16:07:15 +0700

    Create CS01-2.py

---
+++ CS01-2.py
@@ -0,0 +1,12 @@
+# i = 20 
+# print(type(i))
+# nn = "Boss"
+# print(type(nn))
+# a = int(input("Enter ur age : "))
+# n1 = float(input("enter num1  : "))
+# n2 = float(input("enter num2  : "))
+# print(n1 + n2)
+# print("{} + {} = {}".format(n1 , n2, n1 + n2))
+n = input("enter your name : ")
+print(n)
+print(type(n))
