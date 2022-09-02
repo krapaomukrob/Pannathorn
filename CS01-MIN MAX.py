a36c0d733d55b02ffd003cfaeb739f26ac4babca
krapaomukrob <Pannathorn3116@gmail.com>
2022-09-02 16:09:44 +0700

    Update CS01-MIN MAX.py

--- CS01-MIN MAX.py
+++ CS01-MIN MAX.py
@@ -3,8 +3,6 @@ for i in range(int(input("quantity of data : "))):
     n = int(input("Enter data{} : ".format(i+1)))
     totalnum.append(n)
 
-print()
 print(totalnum)
-print()
 print(max(totalnum))
 print(min(totalnum))
